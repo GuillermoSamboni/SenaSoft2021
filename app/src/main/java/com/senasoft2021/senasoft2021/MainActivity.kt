5845ce3e0b9fd88c51dbe2a9e84e0a6ed758c7c8
guillermo <guillermosamboni0912@gmail.com>
2021-10-12 08:58:01 -0500

    init

---
+++ app/src/main/java/com/senasoft2021/senasoft2021/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.senasoft2021.senasoft2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
