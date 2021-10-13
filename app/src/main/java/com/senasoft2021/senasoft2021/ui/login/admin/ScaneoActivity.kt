d209ce60797c4839cc7c49b12128b44b3fda849f
guillermo <guillermosamboni0912@gmail.com>
2021-10-13 11:05:38 -0500

    scaneo activity finish

--- app/src/main/java/com/senasoft2021/senasoft2021/ui/login/admin/ScaneoActivity.kt
+++ app/src/main/java/com/senasoft2021/senasoft2021/ui/login/admin/ScaneoActivity.kt
@@ -2,10 +2,13 @@ package com.senasoft2021.senasoft2021.ui.login.admin
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.huawei.hms.hmsscankit.ScanUtil
 import com.huawei.hms.ml.scan.HmsScan
@@ -22,7 +25,6 @@ class ScaneoActivity : AppCompatActivity() {
         const val DEFAULT_VIEW=0x21
         const val REQUEST_CODE_SCAN=0x01
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,19 +43,36 @@ class ScaneoActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        var optiosn=HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).create()
+        val options=HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).create()
+
         if (requestCode== REQUEST_CODE_SCAN && grantResults.size<2 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1]==PackageManager.PERMISSION_GRANTED){
             return
         }
-        if (requestCode== DEFAULT_VIEW){
-            ScanUtil.startScan(this, REQUEST_CODE_SCAN, optiosn)
+        if (requestCode==DEFAULT_VIEW){
+            ScanUtil.startScan(this as Activity?, REQUEST_CODE_SCAN, options)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==Activity.RESULT_OK || data==null){
+
+        if (requestCode == RESULT_OK || data==null){
             return
+        }
+        if (requestCode==REQUEST_CODE_SCAN){
+            var obj=data.getParcelableExtra(ScanUtil.RESULT) as HmsScan?
+            obj?.showResult
+            val alerDialog= androidx.appcompat.app.AlertDialog.Builder(this)
+            alerDialog.setCancelable(false)
+                .setTitle("RESULTADO DEL SCANEO")
+                .setMessage("Resultado  ${obj?.showResult}")
+                .setPositiveButton("Siguiente"){_,_->
+                    alerDialog.setCancelable(false)
+
+                }
+            alerDialog.create().show()
+
+            Toast.makeText(this, "${obj?.showResult}", Toast.LENGTH_SHORT).show()
         }
     }
 }
