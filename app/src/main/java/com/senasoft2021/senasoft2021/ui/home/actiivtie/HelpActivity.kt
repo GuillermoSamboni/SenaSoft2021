1d40f8c6e423c52bca4c3b9a7803025eb4e7d225
guillermo <guillermosamboni0912@gmail.com>
2021-10-13 20:34:33 -0500

    help

--- app/src/main/java/com/senasoft2021/senasoft2021/ui/home/actiivtie/HelpActivity.kt
+++ app/src/main/java/com/senasoft2021/senasoft2021/ui/home/actiivtie/HelpActivity.kt
@@ -17,15 +17,15 @@ import com.senasoft2021.senasoft2021.huawei.location.LocationService
 
 
 class HelpActivity : AppCompatActivity() {
-    lateinit var binding:ActivityHelpBinding
-    var requesCodeCamera=1
-    var requesCodeStorage=2
-    private var uriImage:Uri?=null
-    private var locationService: LocationService? =null
+    lateinit var binding: ActivityHelpBinding
+    var requesCodeCamera = 1
+    var requesCodeStorage = 2
+    private var uriImage: Uri? = null
+    private var locationService: LocationService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityHelpBinding.inflate(layoutInflater)
+        binding = ActivityHelpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
@@ -34,30 +34,43 @@ class HelpActivity : AppCompatActivity() {
         binding.idBtnCamera.setOnClickListener { openCamera() }
         binding.idBtnStorage.setOnClickListener { openStorage() }
         binding.idBtnSend.setOnClickListener {
-            sendMessageHelp() }
+            sendMessageHelp()
+        }
 
     }
 
     private fun openStorage() {
-        var permisosStorage=ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
-        var intentStorage=Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        var permisosStorage = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        var intentStorage = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
 
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-            if (permisosStorage==PackageManager.PERMISSION_DENIED){
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), requesCodeStorage)
-            }else{
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permisosStorage == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    requesCodeStorage
+                )
+            } else {
                 startActivityForResult(intentStorage, requesCodeStorage)
             }
         }
     }
 
     private fun openCamera() {
-        var permisosCamera=ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-        var intentCamera=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-            if (permisosCamera==PackageManager.PERMISSION_DENIED){
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), requesCodeCamera)
-            }else{
+        var permisosCamera =
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        var intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permisosCamera == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    requesCodeCamera
+                )
+            } else {
                 startActivityForResult(intentCamera, requesCodeCamera)
             }
         }
@@ -68,11 +81,11 @@ class HelpActivity : AppCompatActivity() {
      */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode==requesCodeCamera){
+        if (requestCode == requesCodeCamera) {
 
-        }else if (requestCode==requesCodeStorage){
-            if (resultCode==RESULT_OK && data!=null){
-                uriImage=data.data
+        } else if (requestCode == requesCodeStorage) {
+            if (resultCode == RESULT_OK && data != null) {
+                uriImage = data.data
                 binding.idimageView.setImageURI(uriImage)
             }
         }
@@ -83,47 +96,40 @@ class HelpActivity : AppCompatActivity() {
      */
 
     private fun sendMessageHelp() {
-
-        if (binding.idTxtMessageHelp.text!!.isNotEmpty() && uriImage != null) {
+        if (!(binding.idTxtMessageHelp.text!!.isEmpty() || uriImage != null)) {
             var intentSend = Intent()
             intentSend.action = Intent.ACTION_VIEW
             val defaultNumber = "+57 3163254647"
             var stingMessage = "whatsapp://send?phone=+ $defaultNumber" + "${binding.idTxtMessageHelp.text}"
             intentSend.data = Uri.parse(stingMessage)
-
-            if (binding.idTxtMessageHelp.text!!.isNotEmpty() && uriImage != null) {
-                var intentSend = Intent()
-                intentSend.action = Intent.ACTION_VIEW
-                val defaultNumber = "+57 3163254647"
-                var stingMessage = "whatsapp://send?phone=+ $defaultNumber" + "\n ${binding.idTxtMessageHelp.text}" + Constantes.myLocation
-                intentSend.data = Uri.parse(stingMessage)
-                startActivity(intentSend)
-            } else if (binding.idTxtMessageHelp.text!!.isEmpty() && uriImage == null) {
-                Toast.makeText(this, "holaa", Toast.LENGTH_SHORT).show()
-                var sendintent = Intent()
-                sendintent.action = Intent.ACTION_SEND
-                sendintent.type = "image/*"
-                sendintent.type = "text/plain"
-                sendintent.setPackage("com.whatsapp")
-                if (uriImage != null && binding.idTxtMessageHelp.text.toString().isNotEmpty()) {
-                    sendintent.putExtra(Intent.EXTRA_STREAM, uriImage)
-                    sendintent.putExtra(Intent.EXTRA_TEXT, binding.idTxtMessageHelp.text.toString())
-                    try {
-                        startActivity(sendintent)
-                    } catch (e: Exception) {
-                        Log.d("Error send", e.toString())
-                    }
-                } else {
-                    Toast.makeText(this, "no se eligio ninguna imagen", Toast.LENGTH_SHORT).show()
-                }
+            startActivity(intentSend)
+        } else
+            Toast.makeText(this, "holaa", Toast.LENGTH_SHORT).show()
+        var sendintent = Intent()
+        sendintent.action = Intent.ACTION_SEND
+        sendintent.type = "image/*"
+        sendintent.type = "text/plain"
+        sendintent.setPackage("com.whatsapp")
+ 
+        if (uriImage != null) {
+            sendintent.putExtra(Intent.EXTRA_STREAM, uriImage)
+            sendintent.putExtra(Intent.EXTRA_TEXT, binding.idTxtMessageHelp.text.toString())
+            try {
+                startActivity(sendintent)
+            } catch (e: Exception) {
+                Log.d("Error send", e.toString())
             }
+        } else {
+            Toast.makeText(this, "no se eligio ninguna imagen", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun sendMessgeLocation(){
-        if (locationService==null){
-            locationService= LocationService(this)
+    private fun sendMessgeLocation() {
+        if (locationService == null) {
+            locationService = LocationService(this)
             locationService!!.starRequest()
         }
     }
+
+
 }
