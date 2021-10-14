cc1a74fbc7a63389b144cefae2352cb944f7d7fc
guillermo <guillermosamboni0912@gmail.com>
2021-10-13 19:31:15 -0500

    solucion error

--- app/src/main/java/com/senasoft2021/senasoft2021/ui/home/actiivtie/HelpActivity.kt
+++ app/src/main/java/com/senasoft2021/senasoft2021/ui/home/actiivtie/HelpActivity.kt
@@ -1,7 +1,5 @@
 package com.senasoft2021.senasoft2021.ui.home.actiivtie
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -13,8 +11,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.startActivity
-import com.google.android.material.snackbar.Snackbar
+import com.senasoft2021.senasoft2021.constantes.Constantes
 import com.senasoft2021.senasoft2021.databinding.ActivityHelpBinding
 import com.senasoft2021.senasoft2021.huawei.location.LocationService
 
@@ -30,12 +27,9 @@ class HelpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityHelpBinding.inflate(layoutInflater)
         setContentView(binding.root)
-<<<<<<< HEAD
 
-=======
         supportActionBar?.hide()
         locationService?.starRequest()
->>>>>>> 83567c26419f5c4d53ad9357c495a626fa0e330d
 
         binding.idBtnCamera.setOnClickListener { openCamera() }
         binding.idBtnStorage.setOnClickListener { openStorage() }
@@ -88,42 +82,40 @@ class HelpActivity : AppCompatActivity() {
      * Envia,ops mensaje
      */
 
-    private fun sendMessageHelp(){
-<<<<<<< HEAD
+    private fun sendMessageHelp() {
 
-        if (binding.idTxtMessageHelp.text.isNotEmpty() && uriImage!=null) {
+        if (binding.idTxtMessageHelp.text!!.isNotEmpty() && uriImage != null) {
             var intentSend = Intent()
             intentSend.action = Intent.ACTION_VIEW
             val defaultNumber = "+57 3163254647"
             var stingMessage = "whatsapp://send?phone=+ $defaultNumber" + "${binding.idTxtMessageHelp.text}"
             intentSend.data = Uri.parse(stingMessage)
-=======
-        if (binding.idTxtMessageHelp.text!!.isNotEmpty() && uriImage!=null){
-            var intentSend=Intent()
-            intentSend.action=Intent.ACTION_VIEW
-            val defaultNumber="+57 3163254647"
-            var stingMessage="whatsapp://send?phone=+ $defaultNumber" + "\n ${binding.idTxtMessageHelp.text}" + Constantes.myLocation
-            intentSend.data= Uri.parse(stingMessage)
->>>>>>> 83567c26419f5c4d53ad9357c495a626fa0e330d
-            startActivity(intentSend)
-        }else if(binding.idTxtMessageHelp.text.isEmpty() && uriImage==null){
-            Toast.makeText(this, "holaa", Toast.LENGTH_SHORT).show()
-            var sendintent=Intent()
-            sendintent.action=Intent.ACTION_SEND
-            sendintent.type="image/*"
-            sendintent.type="text/plain"
-            sendintent.setPackage("com.whatsapp")
-            if (uriImage!=null && binding.idTxtMessageHelp.text.toString().isNotEmpty()){
-                sendintent.putExtra(Intent.EXTRA_STREAM, uriImage)
-                sendintent.putExtra(Intent.EXTRA_TEXT, binding.idTxtMessageHelp.text.toString())
-                try{
-                    startActivity(sendintent)
-                }catch (e:Exception){
-                    Log.d("Error send", e.toString())
+
+            if (binding.idTxtMessageHelp.text!!.isNotEmpty() && uriImage != null) {
+                var intentSend = Intent()
+                intentSend.action = Intent.ACTION_VIEW
+                val defaultNumber = "+57 3163254647"
+                var stingMessage = "whatsapp://send?phone=+ $defaultNumber" + "\n ${binding.idTxtMessageHelp.text}" + Constantes.myLocation
+                intentSend.data = Uri.parse(stingMessage)
+                startActivity(intentSend)
+            } else if (binding.idTxtMessageHelp.text!!.isEmpty() && uriImage == null) {
+                Toast.makeText(this, "holaa", Toast.LENGTH_SHORT).show()
+                var sendintent = Intent()
+                sendintent.action = Intent.ACTION_SEND
+                sendintent.type = "image/*"
+                sendintent.type = "text/plain"
+                sendintent.setPackage("com.whatsapp")
+                if (uriImage != null && binding.idTxtMessageHelp.text.toString().isNotEmpty()) {
+                    sendintent.putExtra(Intent.EXTRA_STREAM, uriImage)
+                    sendintent.putExtra(Intent.EXTRA_TEXT, binding.idTxtMessageHelp.text.toString())
+                    try {
+                        startActivity(sendintent)
+                    } catch (e: Exception) {
+                        Log.d("Error send", e.toString())
+                    }
+                } else {
+                    Toast.makeText(this, "no se eligio ninguna imagen", Toast.LENGTH_SHORT).show()
                 }
-            }
-            else{
-                Toast.makeText(this, "no se eligio ninguna imagen", Toast.LENGTH_SHORT).show()
             }
         }
     }
